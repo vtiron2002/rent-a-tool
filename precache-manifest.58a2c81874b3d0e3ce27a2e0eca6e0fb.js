7ca235168765c0fa6822952fe59af680d848be3d
vtiron2002 <vtiron2002@gmail.com>
2020-04-19 21:40:55 -0400

    Updates

---
+++ precache-manifest.58a2c81874b3d0e3ce27a2e0eca6e0fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8c8d9e656a910460b8b0494ba39a8c",
+    "url": "/rent-a-tool/index.html"
+  },
+  {
+    "revision": "b2b0b4433262c4941da2",
+    "url": "/rent-a-tool/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "77c71fa048ad741d40fe",
+    "url": "/rent-a-tool/static/css/main.2990d9cb.chunk.css"
+  },
+  {
+    "revision": "b2b0b4433262c4941da2",
+    "url": "/rent-a-tool/static/js/2.d5a6157b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/rent-a-tool/static/js/2.d5a6157b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c71fa048ad741d40fe",
+    "url": "/rent-a-tool/static/js/main.880dbbc2.chunk.js"
+  },
+  {
+    "revision": "f4a383638f30d86e0448",
+    "url": "/rent-a-tool/static/js/runtime-main.03358c37.js"
+  },
+  {
+    "revision": "eb7fbe77c1835f988763a45dc6a0adbc",
+    "url": "/rent-a-tool/static/media/no_image_placeholder.eb7fbe77.png"
+  }
+]);
